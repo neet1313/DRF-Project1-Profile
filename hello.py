eb359a7bc254d6901070bb8dcefed62b72fbf107
Prabhneet <prabhneet13@gmail.com>
2023-04-26 05:17:53 +0530

    Created Django project and app

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
 print('Hello world')
+# ----> Making python virtual environment:
+# 1) Start the Vagrant Server-> vagrant ssh
+# 2) Go to Vagrant Local Directory-> cd /vagrant/
+# 3)Make python virtual environment in vagrant HOME directory-> python -m venv ~/env
+# 4)Activate the virtual environment-> source ~/env/bin/activate 
+# 5)Deactivating virtual environment command -> deactivate
