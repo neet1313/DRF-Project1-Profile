effb0a8d69065257d6954516e6e2cbd73e61e59a
Prabhneet <prabhneet13@gmail.com>
2023-04-26 21:50:33 +0530

    admin superuser created

--- Profiles_API/admin.py
+++ Profiles_API/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+
+class UserprofileAdmin(admin.ModelAdmin):
+    list_display= ('email', 'name', 'is_active', 'is_staff')
+
+admin.site.register(UserProfile)
