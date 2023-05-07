f60e7df05a04abb6a3aa49c6c1a736033898cc0f
Prabhneet <prabhneet13@gmail.com>
2023-05-07 07:45:12 +0530

    Registering Viewset with URL

--- Profiles_API/urls.py
+++ Profiles_API/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from Profiles_API import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+# Since in views.py 'queryset' property is set so 'basename' parameter
+# will be picked up from there automatically and we don't need to specify it
+# in this case
+router.register('profile', views.UserProfileViewset)
 
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view())
+    path('hello-view/', views.HelloApiView.as_view()),
+    # Registering Viewset to routers for DRF to take care of it automatically
+    path('', include(router.urls))
 ]
